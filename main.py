9e9cea60c0b73527c2bc4375223efedac53769fe
Dao Heng Liu <daohengliu@gmail.com>
2025-05-19 21:41:39 +0100

    fix token fetch, add room data fetching with rate limit

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
+import datetime
 import os
+import time
 
 from fastapi import FastAPI
-import cachetools.func
+from aiocache import cached
 from dotenv import load_dotenv
-import requests
+import httpx
 
+from aiolimiter import AsyncLimiter
 
 load_dotenv()
 
+aiolimit = AsyncLimiter(60, 60)
 app = FastAPI()
 
-
-@cachetools.func.ttl_cache(maxsize=1, ttl=3600)
-async def get_access_token_cached():
-    return await get_access_token()
-
-
+@cached(key="osu_token", ttl=3600)
 async def get_access_token():
     access_key = os.getenv("OSU_ACCESS_KEY")
     access_secret = os.getenv("OSU_ACCESS_SECRET")
@@ -26,29 +25,74 @@ async def get_access_token():
 
     token_endpoint = "https://osu.ppy.sh/oauth/token"
 
-    resp = requests.post(token_endpoint,
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(token_endpoint,
                          auth=(access_key, access_secret),
                          data={
                              "grant_type": "client_credentials",
                              "scope": "public"
                          })
 
-    if resp.status_code not in range(200, 300):
-        print(f"token response returned {resp.status_code}")
-        return None
+        if resp.status_code not in range(200, 300):
+            print(f"token response returned {resp.status_code}")
+            return None
 
-    return resp.json()["access_token"]
+        return resp.json()["access_token"]
 
 
-async def make_osu_request(endpoint: str, payload: dict):
-    pass
+@cached(ttl=3600, key_builder=lambda f, *args, **kwargs: f"{args[0]}_{args[1]}")
+async def make_osu_request(method: str, endpoint: str, token: str):
+    async with aiolimit:
+        request = httpx.Request(
+            method,
+            endpoint,
+            headers={"Authorization": f"Bearer {token}"}
+        )
 
+        print(f"[{datetime.datetime.now().isoformat()}] {method.upper()} {endpoint}")
+
+        async with httpx.AsyncClient() as client:
+            return await client.send(request)
 
 @app.get("/")
 async def root():
     return {"message": "hi, go to /multiplayer/rooms/<room_id> to look at a match"}
 
+PLAYLIST_KEYS = ["played_at", "id", "beatmap"]
+
 # https://osu.ppy.sh/multiplayer/rooms/1368095
 @app.get("/multiplayer/rooms/{room_id}")
 async def say_hello(room_id: str):
-    return {"message": f"Hello {room_id}"}
+    access_token = await get_access_token()
+
+    room_request = await make_osu_request("GET", f"https://osu.ppy.sh/api/v2/rooms/{room_id}", access_token)
+
+    if room_request.status_code != 200:
+        return {"error": f"failed to fetch resource: {room_request.status_code}"}
+
+    room_data = room_request.json()
+
+    readable_playlist_items = []
+
+    if "playlist" not in room_data:
+        return {"error": "room data did not contain a playlist"}
+
+    for playlist_item in room_data["playlist"]:
+        new_playlist_item = {key: playlist_item[key] for key in PLAYLIST_KEYS}
+
+        scores_req = await make_osu_request("GET",
+                                      f"https://osu.ppy.sh/api/v2/rooms/{room_id}/playlist/{new_playlist_item['id']}/scores",
+                                      access_token)
+
+        if scores_req.status_code != 200:
+            scores = {"error": f"failed fetching scores "
+                               f"for playlist {new_playlist_item['id']}: {scores_req.status_code}"}
+        else:
+            scores = scores_req.json()
+
+
+        new_playlist_item["scores"] = scores
+
+        readable_playlist_items.append(new_playlist_item)
+
+    return {"name": room_data["name"], "playlist": readable_playlist_items}
